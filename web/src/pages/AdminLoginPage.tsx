a047ab1a67bebef828e7abe2a038766ffbc5739f
hemanths004 <pes2ug23cs220@pesu.pes.edu>
2026-06-12 12:03:16 +0530

    Disable OneTap and force account selection for Google Login

--- web/src/pages/AdminLoginPage.tsx
+++ web/src/pages/AdminLoginPage.tsx
@@ -66,7 +66,7 @@ export const AdminLoginPage: React.FC = () => {
                 onError={() => {
                   toast.error('Google Login Failed');
                 }}
-                useOneTap
+                prompt="select_account"
                 theme="filled_black"
                 shape="pill"
                 size="large"
